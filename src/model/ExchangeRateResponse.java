b7027fbb03cf425afed533fb892b6ebcc4005113
castaneda <juxnpxblojpcd315@gmail.com>
2026-02-23 12:18:41 -0500

    Atributos de la clase modelo basados en la respuesta de a la consulta en Postman

--- src/model/ExchangeRateResponse.java
+++ src/model/ExchangeRateResponse.java
@@ -2,4 +2,9 @@ package model;
 
 public class ExchangeRateResponse {
 
+    private String result;
+    private String base_code;
+    private String target_code;
+    private double conversion_rate;
+
 }
