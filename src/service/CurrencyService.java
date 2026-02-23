4b256565ff2c0fc927b0c07e076ea0410f5a06f7
castaneda <juxnpxblojpcd315@gmail.com>
2026-02-23 15:57:58 -0500

    Comentarios de la Clase service para un codigo mas entendible para el Desarrollador

--- src/service/CurrencyService.java
+++ src/service/CurrencyService.java
@@ -6,12 +6,19 @@ import model.ExchangeRateResponse;
 public class CurrencyService {
     private final ExchangeRateClient client = new ExchangeRateClient();
 
+
     public void executeConversion(String base, String target, double amount) {
         try {
+            //manejo de error para que el usuario no introduzca un monto no valído
+            // Validación de Regla de Negocio
             if (amount <= 0){
                 throw new IllegalArgumentException("El monto debe ser mayor que cero");
             }
+
+            // El servicio no sabe cómo funciona el HTTP, solo le pide al 'client' el resultado.
             ExchangeRateResponse response = client.searchConversion(base, target, amount);
+
+            // Formateo de Salida
             System.out.printf(">>> El valor %.2f [%s] corresponde al valor final de =>>> %.2f [%s]%n",
                     amount, base, response.conversion_result(), target);
         } catch (Exception e) {
