564c03443c5697f828056fe2d7818102425f47a2
castaneda <juxnpxblojpcd315@gmail.com>
2026-02-23 15:42:27 -0500

    Manejo de un error nuevo previniendo que el usuario ingrese valores negativos

--- src/service/CurrencyService.java
+++ src/service/CurrencyService.java
@@ -8,6 +8,9 @@ public class CurrencyService {
 
     public void executeConversion(String base, String target, double amount) {
         try {
+            if (amount <= 0){
+                throw new IllegalArgumentException("El monto debe ser mayor que cero");
+            }
             ExchangeRateResponse response = client.searchConversion(base, target, amount);
             System.out.printf(">>> El valor %.2f [%s] corresponde al valor final de =>>> %.2f [%s]%n",
                     amount, base, response.conversion_result(), target);
