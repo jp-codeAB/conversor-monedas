b9b944c4ed681bf5fdeb4d6da92782c0fa2259fc
castaneda <juxnpxblojpcd315@gmail.com>
2026-02-23 15:46:32 -0500

    Menu de opciones interación con el usuario

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,54 @@
 
+import service.CurrencyService;
+import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        CurrencyService service = new CurrencyService();
+        int option = 0;
 
+        String menu = """
+                ***************************************************
+                      Sea bienvenid@ al Conversor de Monedas
+                
+                1) Dólar ->> Peso argentino
+                2) Peso argentino ->> Dólar
+                3) Dólar ->> Real brasileño
+                4) Real brasileño ->> Dólar
+                5) Dólar =>> Peso Colombiano❤
+                6) Peso Colombiano❤ ->> Dólar
+                7) Dólar ->> Euro
+                8) Euro ->> Dólar
+                9) Salir
+                Elija una opción válida:
+                ***************************************************
+                """;
+
+        while (option != 9) {
+            System.out.print(menu);
+            try {
+                option = Integer.parseInt(sc.nextLine());
+                if (option == 9) break;
+
+                System.out.print("Ingrese el valor que desea convertir: ");
+                double amount = Double.parseDouble(sc.nextLine());
+
+                switch (option) {
+                    case 1 -> service.executeConversion("USD", "ARS", amount);
+                    case 2 -> service.executeConversion("ARS", "USD", amount);
+                    case 3 -> service.executeConversion("USD", "BRL", amount);
+                    case 4 -> service.executeConversion("BRL", "USD", amount);
+                    case 5 -> service.executeConversion("USD", "COP", amount);
+                    case 6 -> service.executeConversion("COP", "USD", amount);
+                    case 7 -> service.executeConversion("USD", "EUR", amount);
+                    case 8 -> service.executeConversion("EUR", "USD", amount);
+                    default -> System.out.println("Opción no válida.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Por favor, ingrese un número válido.");
+            }
+        }
+        System.out.println("Programa finalizado. ¡Gracias!");
     }
 }
