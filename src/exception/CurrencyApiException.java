c1da64ee9601657ae4fa8c0166dbaa8f27751459
castaneda <juxnpxblojpcd315@gmail.com>
2026-02-23 15:21:44 -0500

    Manejo de codigo limpio clase del servicio

--- src/exception/CurrencyApiException.java
+++ src/exception/CurrencyApiException.java
@@ -9,3 +9,4 @@ public class CurrencyApiException extends RuntimeException {
     public CurrencyApiException(String message, Throwable cause) {
         super(message, cause);
     }
+}
--- src/service/CurrencyService.java
+++ src/service/CurrencyService.java
@@ -1,4 +1,18 @@
 package service;
 
+import client.ExchangeRateClient;
+import model.ExchangeRateResponse;
+
 public class CurrencyService {
+    private final ExchangeRateClient client = new ExchangeRateClient();
+
+    public void executeConversion(String base, String target, double amount) {
+        try {
+            ExchangeRateResponse response = client.searchConversion(base, target, amount);
+            System.out.printf(">>> El valor %.2f [%s] corresponde al valor final de =>>> %.2f [%s]%n",
+                    amount, base, response.conversion_result(), target);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
